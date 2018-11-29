71f6f8eb82290193bb1379e8ff626777591bfdf8
Nico <nicovigil1@gmail.com>
2018-11-28 21:18:33 -0700

    Adds passing test for displaying Specials on index

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -2,6 +2,7 @@ class LaughTracksApp < Sinatra::Base
     get '/comedians' do 
         @comedians = Comedian.all
         @specials = Special.all
+        
         erb :index
     end 
 end
--- spec/features/user_can_see_comedians_specials_spec.rb
+++ spec/features/user_can_see_comedians_specials_spec.rb
@@ -1,15 +1,15 @@
 RSpec.describe "It can display Specials" do
 
     context "it can display Comedian's specials under their name"
-    Comedian.create(name: "Cesar Chavez", age: 66, city: "San Jose")
-    Comedian.create(name: "Dan Briechle", age: 30, city: "Flemington")
-    Comedian.create(name: "Will Petty", age: 45, city: "Nuremburg")
-    Comedian.create(name: "Bradley Niedt", age: 66, city: "Denver")
-
-    Special.create(name: "UFW", runtime: 35215200)
-    Special.create(name: "Delano grape strike", runtime: 2628000)
-
     it 'on the page' do
+        Comedian.create(name: "Cesar Chavez", age: 66, city: "San Jose")
+        Comedian.create(name: "Dan Briechle", age: 30, city: "Flemington")
+        Comedian.create(name: "Will Petty", age: 45, city: "Nuremburg")
+        Comedian.create(name: "Bradley Niedt", age: 66, city: "Denver")
+    
+        Special.create(name: "UFW", runtime: 35215200)
+        Special.create(name: "Delano grape strike", runtime: 2628000)
+    
         
         visit '/comedians'
 
