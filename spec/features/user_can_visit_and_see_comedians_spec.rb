084b5329f0e0b42c09e84ac14ee355f7662afb77
Nico <nicovigil1@gmail.com>
2018-11-28 18:24:09 -0700

    Adds test for each comedian to be in their own div with class comedian

--- spec/features/user_can_visit_and_see_comedians_spec.rb
+++ spec/features/user_can_visit_and_see_comedians_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe 'visit and see comedians page' do
     context 'visit comedians' do
-       it 'can visit comedians' do  
+       it 'can visit and view comedians' do  
         Comedian.create(name: "Cesar Chavez", age: "66", city: "San Jose")
 
         visit '/comedians'  
@@ -8,6 +8,18 @@ RSpec.describe 'visit and see comedians page' do
         expect(page).to have_content('Cesar Chavez')
         expect(page).to have_content('66')
         expect(page).to have_content('San Jose')
+       end
+
+       it 'places comedians in their own div' do
+        Comedian.create(name: "Cesar Chavez", age: "66", city: "San Jose")
+        Comedian.create(name: "Dan Briechle", age: "30", city: "Flemington")
+        Comedian.create(name: "Will Petty", age: "45", city: "Nuremburg")
+
+        visit '/comedians'
+        
+        within('div.comedian') do
+            expect(page).to have_content('Cesar Chavez')
+        end 
        end 
     end 
 end 
